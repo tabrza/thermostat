dfcc3b7b0fe84907da311faa4d19c9853cb5fa49
Tabish Raza <tabishraza@gmail.com>
2017-11-22 15:36:00 +0000

    temperature can be decreased but not more than min temp

--- src/Thermostat.js
+++ src/Thermostat.js
@@ -1,9 +1,18 @@
 var temperatureStart = 20;
+const minTemperature = 10;
 
 function Thermostat(){
   this.temperature = temperatureStart;
+  this.minTemperature = minTemperature;
 };
 
 Thermostat.prototype.temperatureUp = function(degrees) {
   return this.temperature += degrees;
+};
+
+Thermostat.prototype.temperatureDown = function(degrees){
+  if((this.temperature - degrees) < minTemperature){
+    return "Min temperature is 10 degrees";
+  }
+  return this.temperature -= degrees;
 };
