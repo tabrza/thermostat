5bb5791a553169eca66ad0f31c596312d64a2ce5
Marie Kerkstoel <mariekerkstoel@gmail.com>
2017-11-22 15:53:05 +0000

    Max temp for power saving modes implemented

--- src/Thermostat.js
+++ src/Thermostat.js
@@ -1,13 +1,26 @@
 var temperatureStart = 20;
 const minTemperature = 10;
+const maxSaving = 25;
+const maxTemp = 32;
 
 function Thermostat(){
   this.temperature = temperatureStart;
   this.minTemperature = minTemperature;
+  this.powerSavingMode = true;
 };
 
 Thermostat.prototype.temperatureUp = function(degrees) {
-  return this.temperature += degrees;
+  if (this.powerSavingMode === true) {
+    if (this.temperature + degrees > maxSaving){
+      return 'Max temp in power saving mode is 25 degrees'
+    }
+    return this.temperature += degrees;
+  } else {
+      if (this.temperature + degrees > maxTemp){
+        return 'Max temp is 32 degrees';
+      }
+      return this.temperature += degrees;
+  };
 };
 
 Thermostat.prototype.temperatureDown = function(degrees){
